7cfa422177c568d6bf2b249dc253a820dd3e730d
kik_renova_ai <kik87827@gmail.com>
2026-07-05 21:11:03 +0900

    스터디 교육중

--- func.js
+++ func.js
@@ -10,5 +10,19 @@ function Func3(x) {
         console.log(x + 3);
     }
 }
+function Func4(x) {
+    if (typeof x === 'string') {
+        return 'x' + 2;
+    }
+    return x + 2;
+}
+function Func5(x) {
+    let arr = [];
+    /* if(typeof x === "number"){
+      arr[0] = x;
+    } */
+    arr[0] = x;
+}
 Func(30);
+Func5(30);
 Func2();
--- func.ts
+++ func.ts
@@ -12,5 +12,21 @@ function Func3(x : number | string) :void {
   }
 }
 
+function Func4(x : number | string) :number | string {
+  if(typeof x === 'string') {
+     return 'x' + 2;
+  }
+  return x + 2;
+}
+
+function Func5 (x :number|string){
+  let arr :number[] = [];
+  /* if(typeof x === "number"){
+    arr[0] = x;
+  } */
+ arr[0] = x as number;
+}
+
 Func(30);
+Func5(30);
 Func2();  
