43ca693ded5676b675dab1961f5c0c55068e84f5
kik_renova_ai <kik87827@gmail.com>
2026-07-08 18:24:34 +0900

    강의 1개 다듣고 dom 숙제 남음

--- dom.ts
+++ dom.ts
@@ -1,7 +1,19 @@
 export default function domControl(){
     console.log('dom');
     const title = document.querySelector("#title");
+    const link = document.querySelector(".link");
+    const button = document.querySelector("#button");
     // optional chaining
+
     if(title?.innerHTML)
     title.innerHTML = '반가워요';
+
+    if(link instanceof HTMLAnchorElement){
+        link.href="http://kakao.com"
+    }
+
+    // 버튼에 addEventListener 가능하면 해주시고 아니면 undefined 뱉어
+    button?.addEventListener("click",()=>{
+
+    })
 }
