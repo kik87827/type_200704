e2079ef1569deeae97b1a479bb731f7c57880ef5
kik_renova_ai <kik87827@gmail.com>
2026-07-09 10:04:23 +0900

    cons 진행중

---
+++ constructor.ts
@@ -0,0 +1,17 @@
+// 객체지향
+// constructor - 비슷한 object를 많이 복사할때 사용
+
+// var stu1 = { name : 'kim', age : 15}
+// var stu2 = { name : 'lee', age : 15}
+
+export default function constructorStudy(){
+    function Student(name,age) {
+        this.name = name;
+        this.age = age;
+        this.print = () => console.log('hi');
+    }
+    
+    var stu1 = new Student('kim','12');
+    stu1.print();
+    console.log(stu1);
+}
